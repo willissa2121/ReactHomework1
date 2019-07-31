876a44e8cdf8c0682f0bc2ffb4a5a155169f30fb
willissa2121 <you@example.com>
2019-07-31 08:52:23 -0700

    Updates

---
+++ precache-manifest.0a43f509857eb3dbabdf8a96cf93102c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec6a3da17728b0536bb01893b0eab28",
+    "url": "/ReactHomework1/index.html"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ReactHomework1/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "bb5eb6bb2dd5057f101a",
+    "url": "/ReactHomework1/static/js/main.baaea17c.chunk.js"
+  },
+  {
+    "revision": "f75fd928f5536f82e765",
+    "url": "/ReactHomework1/static/js/runtime~main.77e00f8e.js"
+  }
+]);
